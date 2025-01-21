715ccd9edafa101e90ed28e2450b67db459a6919
MeV2V2 <jaeminp707@gmail.com>
2025-01-21 23:10:37 +1100

    Added response functionality

--- tracker_bot.py
+++ tracker_bot.py
@@ -8,9 +8,12 @@ bot = discord.Bot()
 async def on_ready():
     print(f'Tracker be tracking - {bot.user}')
 
-@bot.slash_command(name='rank', description='displays player rank')
+@bot.command(name='rank', description='displays player rank')
 async def rank(ctx):
-    pass
+    await ctx.respond('Input your in-game name in the form NAME#TAG')
+    ingame_name = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
+
+    await ctx.send(ingame_name)
 
 dotenv.load_dotenv()
 token = str(os.getenv('BOT_TOKEN'))
