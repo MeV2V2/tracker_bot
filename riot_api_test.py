2b3044e1e99fba24b95a8c0864f5ce75db317a5b
MeV2V2 <jaeminp707@gmail.com>
2025-01-21 19:42:53 +1100

    request with api key

--- riot_api_test.py
+++ riot_api_test.py
@@ -8,8 +8,9 @@ name = os.getenv('GAME_NAME')
 tag = os.getenv('GAME_TAG')
 
 url = f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}"
+full_url = url + '?api_key=' + os.getenv('RIOT_API_TOKEN')
 
-res = requests.get(url)
+res = requests.get(full_url)
 
 print(res)
 
