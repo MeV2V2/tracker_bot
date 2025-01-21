8d7d5f773d117a928a7783e29b4a3c04ce17c2b9
MeV2V2 <jaeminp707@gmail.com>
2025-01-21 22:55:27 +1100

    barebones implementation of rank command

--- tracker_bot.py
+++ tracker_bot.py
@@ -8,6 +8,10 @@ bot = discord.Bot()
 async def on_ready():
     print(f'Tracker be tracking - {bot.user}')
 
+@bot.slash_command(name='rank', description='displays player rank')
+async def rank(ctx):
+    pass
+
 dotenv.load_dotenv()
 token = str(os.getenv('BOT_TOKEN'))
 bot.run(token)
