132e61c08a57ba6e62401ef99264546355693c07
MeV2V2 <jaeminp707@gmail.com>
2025-01-23 10:44:08 +1100

    Synced slash commands

--- tracker_bot.py
+++ tracker_bot.py
@@ -1,19 +1,30 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 import dotenv
 import os
 
-bot = discord.Bot()
+bot = discord.Bot(intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f'Tracker be tracking - {bot.user}')
 
-@bot.command(name='rank', description='displays player rank')
+    # Syncs slash commands with discord API
+    try:
+        synced = await bot.tree.sync()
+        print(f'{len(synced)} commands synced.')
+    except Exception as exception:
+        print(f'Error syncing commands: {exception}')
+
+@bot.slash_command(name='rank', description='displays player rank', guild_ids=[1329958640022585384])
 async def rank(ctx):
     await ctx.respond('Input your in-game name in the form NAME#TAG')
     ingame_name = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
 
-    await ctx.send(ingame_name)
+    print(str(ingame_name.content))
+
+    await ctx.respond(ingame_name)
 
 dotenv.load_dotenv()
 token = str(os.getenv('BOT_TOKEN'))
