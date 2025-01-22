f045fcc691fedc6b65218bbd5422775819aab249
MeV2V2 <jaeminp707@gmail.com>
2025-01-23 10:50:06 +1100

    Moved guild definition outside of command

--- tracker_bot.py
+++ tracker_bot.py
@@ -4,7 +4,14 @@ from discord.ext import commands
 import dotenv
 import os
 
-bot = discord.Bot(intents=discord.Intents.all())
+dotenv.load_dotenv()
+
+# Defining required intents for bot
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
+
+# Defining the guild the bot is to operate in
+guild = discord.Object(id=int(os.getenv('GUILD_ID')))
 
 @bot.event
 async def on_ready():
@@ -12,12 +19,12 @@ async def on_ready():
 
     # Syncs slash commands with discord API
     try:
-        synced = await bot.tree.sync()
+        synced = await bot.tree.sync(guild=guild)
         print(f'{len(synced)} commands synced.')
     except Exception as exception:
         print(f'Error syncing commands: {exception}')
 
-@bot.slash_command(name='rank', description='displays player rank', guild_ids=[1329958640022585384])
+@bot.tree.command(name='rank', description='displays player rank', guild_ids=[1329958640022585384])
 async def rank(ctx):
     await ctx.respond('Input your in-game name in the form NAME#TAG')
     ingame_name = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
@@ -26,6 +33,5 @@ async def rank(ctx):
 
     await ctx.respond(ingame_name)
 
-dotenv.load_dotenv()
 token = str(os.getenv('BOT_TOKEN'))
 bot.run(token)
