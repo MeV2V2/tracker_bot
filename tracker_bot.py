4b9ad0922802aa221f1d52c0ea18c0d05f72efbf
MeV2V2 <jaeminp707@gmail.com>
2025-01-23 12:10:23 +1100

    Re-added on_ready function

--- tracker_bot.py
+++ tracker_bot.py
@@ -7,9 +7,11 @@ dotenv.load_dotenv()
 
 # Defining required intents for bot
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = discord.Bot(command_prefix='$', intents=intents)
 
-
+@bot.event
+async def on_ready():
+    print(f'Tracker be tracking - {bot.user}')
 
 token = str(os.getenv('BOT_TOKEN'))
 bot.run(token)
