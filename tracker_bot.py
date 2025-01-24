f688e9846b13f5364435a06556c2018c2afe1d15
MeV2V2 <jaeminp707@gmail.com>
2025-01-24 14:33:44 +1100

    Added helper function to get player puuid

--- tracker_bot.py
+++ tracker_bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import dotenv
 import os
 import asyncio
+import requests
+import json
 
 dotenv.load_dotenv()
 
@@ -25,9 +27,28 @@ async def register(ctx):
 
     try:
         response = await bot.wait_for('message', timeout=30.0, check=check)
+
+        # Checking appropriate format
+        if response.content.count('#') == 1:
+            name, tag = response.content.split('#')
+        else:
+            raise ValueError
+        
     except asyncio.TimeoutError:
         await ctx.send("You have been timed out. Please try again.") 
 
+    except ValueError:
+        await ctx.send("Please ensure you have inputted your in-game name in the appropriate format.")
+
+# Helper function to fetch puuid from RIOT API
+# RIOT API token is only valid daily. Next expiry: 25-01-2025 2:30pm AEST
+def getd_puuid(name: str, tag: str):
+    url = f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}"
+    full_url = url + '?api_key=' + os.getenv('RIOT_API_TOKEN')
+
+    res = requests.get(full_url)
+
+    return res.json()['puuid']
     
 
 
