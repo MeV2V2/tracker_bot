4f3b0b2ee281bfc116c7588d48b6cea5d725c120
MeV2V2 <jaeminp707@gmail.com>
2025-01-24 13:56:49 +1100

    Updated intents to enable messages and rellocated check function

--- tracker_bot.py
+++ tracker_bot.py
@@ -7,11 +7,25 @@ dotenv.load_dotenv()
 
 # Defining required intents for bot
 intents = discord.Intents.all()
-bot = discord.Bot(command_prefix='$', intents=intents)
+intents.messages = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Tracker be tracking - {bot.user}')
+
+@bot.command(name='register', description='Register a new user into the database.')
+async def register(ctx):
+    await ctx.send('Input your in-game name in the form NAME#TAG: ')
+
+    def check(message: discord.Message):
+        return message.author == ctx.author and message.channel == ctx.channel
+
+    response = await bot.wait_for('message', timeout=30.0, check=check)
+
+    await ctx.send("test")
+
+
 
 token = str(os.getenv('BOT_TOKEN'))
 bot.run(token)
