364da4d957937596d026909510f402c4533d113e
MeV2V2 <jaeminp707@gmail.com>
2025-01-25 16:40:44 +1100

    Rank test

--- riot_api_test.py
+++ riot_api_test.py
@@ -6,11 +6,17 @@ dotenv.load_dotenv()
 
 name = os.getenv('GAME_NAME')
 tag = os.getenv('GAME_TAG')
+puuid = os.getenv('PUUID')
+act_id_5 = '5adc33fa-4f30-2899-f131-6fba64c5dd3a'
+act_id_6 = '4c4b8cff-43eb-13d3-8f14-96b783c90cd2'
 
 url = f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}"
 full_url = url + '?api_key=' + os.getenv('RIOT_API_TOKEN')
 
-res = requests.get(full_url)
+match_url = f"https://ap.api.riotgames.com/val/ranked/v1/leaderboards/by-act/{act_id_6}" + '?api_key=' + os.getenv('RIOT_API_TOKEN')
+content_url = f"https://ap.api.riotgames.com/val/content/v1/contents" + '?api_key=' + os.getenv('RIOT_API_TOKEN')
+
+res = requests.get(match_url)
 
 # HTTP Status Code
 print(res)
